38671fa48218da8059a6478c2add061a070edd99
Arsen <arsik077@mail.ru>
2022-02-17 21:32:26 +0600

    Initial version of truevocation generated by generator-jhipster@7.6.0

---
+++ src/main/java/com/truevocation/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.truevocation.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Truevocation.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/main/java/com/truevocation/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.truevocation.service.dto;
